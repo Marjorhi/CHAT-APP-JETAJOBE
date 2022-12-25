e8aa38987fade028c20d19f7357b51cd5e600e10
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 22:22:35 +0800

    Adding join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,7 +14,7 @@ socket.on('message', (message) => {
     console.log(message)
     const html = Mustache.render(messageTemplate, {
         message: message.text,
-        createdAt: moment(message.createdAt).format('h:mm a')
+        createdAt: moment(message.createdAt).format('h:mm:ss a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
@@ -23,7 +23,7 @@ socket.on('locationMessage', (message) => {
     console.log(message)
     const html = Mustache.render(locationMessageTemplate, {
         url : message.url,
-        createdAt: moment(message.createdAt).format('h:mm a')
+        createdAt: moment(message.createdAt).format('h:mm:ss a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
