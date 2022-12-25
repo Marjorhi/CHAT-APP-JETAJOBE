e8cff93b772468fb15d3705b44e964255ed316b7
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 19:43:14 +0800

    Rendering to the browser

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,9 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
