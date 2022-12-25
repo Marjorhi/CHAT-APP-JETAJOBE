521c0f5400130343545ae0fdf576990e3d7ba547
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 12:30:07 +0800

    Passong the server

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join (__dirname, '../public')
