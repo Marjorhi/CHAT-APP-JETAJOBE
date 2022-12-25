5fccdc4b9be1c23c697afdb094566b2488f97509
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 16:21:14 +0800

    Sharing location from client to server

--- src/index.js
+++ src/index.js
@@ -25,6 +25,10 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `Location: ${coords.latitude}, ${coords.longitude}`)
+    })
+
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left!')
     })
