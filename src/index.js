624759d75a39871ac9540cb0d1858fd0e8fb2f29
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 12:26:17 +0800

    Passing to express application

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require ('express')
 
 const app = express()
-const server = http.createServer
+const server = http.createServer(app)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join (__dirname, '../public')
