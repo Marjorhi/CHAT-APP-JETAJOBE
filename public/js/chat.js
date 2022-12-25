f3ea2b27d0c38d3bbea6c091725b2affc90bb665
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 20:06:43 +0800

    Rendering dynamic template

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,10 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
