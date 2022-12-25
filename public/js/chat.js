5a8f964ebe0403430c8ea31ba5c5d56c153c7172
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 13:48:29 +0800

    Server is doing something when send message is received

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,10 @@ socket.on('message', (message) => {
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
 })
 
 // socket.on('countUpdated', (count) => {
