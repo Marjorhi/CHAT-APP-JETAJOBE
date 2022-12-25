5d1da516123a8edff2c22d79ddc072c9b7b60baf
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 12:29:12 +0800

    Configuring socket.io

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const http = require('http')
 const express = require ('express')
+const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const io = socketio
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join (__dirname, '../public')
@@ -11,6 +13,6 @@ const publicDirectoryPath = path.join (__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
