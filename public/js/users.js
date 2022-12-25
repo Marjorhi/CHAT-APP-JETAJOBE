cf07b85d3080a03216436e4e8d5f79673defb467
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-25 23:06:57 +0800

    Validating the data

--- public/js/users.js
+++ public/js/users.js
@@ -3,4 +3,10 @@ const users = []
 const addUser = ({id, username, room}) => {
     username = username.trim.toLowerCase()
     room = room.trim.toLowerCase()
+
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
 }
