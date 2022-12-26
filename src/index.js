3ab696602662a5c9fdab73c547dc3205e0df61c6
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-26 12:25:02 +0800

    Adding static username admin

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`))
 
         callback()
     })
