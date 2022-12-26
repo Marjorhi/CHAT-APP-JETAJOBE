b9e82550ab7d3c1423c8e5d311c01ca339534ce3
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-26 12:26:50 +0800

    Using admin for system message

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed')
         }
         
-        io.to(user.room).emit('message', generateMessage(message)) 
+        io.to(user.room).emit('message', generateMessage(user.username, message)) 
         callback()
     })
 
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+            io.to(user.room).emit('message', generateMessage('Admin',`${user.username} has left!`))
         }
     })
 })
