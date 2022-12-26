566e6db6755cb19de7fa974d630195a271772307
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-26 14:00:38 +0800

    Setting a new value for How far have I scrolled?

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,14 +17,26 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 const {username, room}= Qs.parse(location.search, { ignoreQueryPrefix : true })
 
 const autoscroll = () => {
+    //New message element
     const $newMessage = $messages.lastElementChild
 
+    //Height of the new message
     const newMessageStyles = getComputedStyle($newMessage)
     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
 
+    //Visible height
     const visibleHeight = $messages.offsetHeight
-}
+
+    //Height of message container
+    const containerHeight = $messages.scrollHeight
+
+    //How far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+    if (containerHeight - newMessageHeight <= scrollOffset) {
+        $messages.scrollTop =$messages.scrollHeight
+    }
+} 
 
 socket.on('message', (message) => {
     console.log(message)
