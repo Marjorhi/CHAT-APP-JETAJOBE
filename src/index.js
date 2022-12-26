2cdd34967b5f94271e7e306d345614d8314f8c4c
Jewoon Macario <jewoonmacario@gmail.com>
2022-12-26 12:03:35 +0800

    Chaining the method call

--- src/index.js
+++ src/index.js
@@ -50,7 +50,8 @@ io.on('connection', (socket) => {
 
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
